1494d2e4c0131fe9452f62bcef71ec7d5323778b
HelenSathvika <70305997+HelenSathvika@users.noreply.github.com>
2025-06-22 18:43:54 +0530

    Update ConnectionSetup.py

--- autoPerf++-server-side-agent/ConnectionSetup.py
+++ autoPerf++-server-side-agent/ConnectionSetup.py
@@ -1,20 +1,26 @@
 import socket
 import json
 
-class connectionSetup:
+class ConnectionSetup:
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Open Socket 
     IPAdress = ""
     port = 0
-    s.bind((IPAdress, port)) #Bind to this IP Address and Port
-    s.listen(1) #Start Listening
-    c, addr = s.accept() #Accept Connection
+    s=""
+    c=""
     data=""
-    condition=True
-    c.send("Connected to server".encode('utf-8'))
+    condition=False
+    def initialize(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Open Socket 
+        self.s.bind((self.IPAdress, self.port)) #Bind to this IP Address and Port
+        self.s.listen(1) #Start Listening
+        self.c, addr = self.s.accept() #Accept Connection
+        self.condition=True
+        self.c.send("Connected to server".encode('utf-8'))
 
     def receiveData(self):
         self.data=self.c.recv(1024).decode('utf-8')
         self.data=json.loads(self.data)
         self.c.send("Received Data".encode('utf-8'))
         return True
+
+
