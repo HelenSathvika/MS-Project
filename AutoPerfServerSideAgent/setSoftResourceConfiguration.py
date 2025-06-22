d146a7a64c3df8921e7d33ce49cc02fb1c5c3c9d
HelenSathvika <70305997+HelenSathvika@users.noreply.github.com>
2025-06-22 09:27:49 +0530

    Create setSoftResourceConfiguration.py

---
+++ AutoPerfServerSideAgent/setSoftResourceConfiguration.py
@@ -0,0 +1,21 @@
+import subprocess
+import re
+import time
+import os
+
+class setSoftResourceConfiguration:
+    data=["path","regExp","value","command","sleep_time"]
+    getData=[]
+    password=""
+
+    def setValue(self): #Set Value
+        command=['sudo', '-S', 'cat', self.data[0]]
+        result = subprocess.run(command, input=self.password, stdout=subprocess.PIPE, text=True) #Read the file content
+        file_content=result.stdout
+        modified_content=re.sub(rf'{self.data[1]}', rf'\g<1>{self.data[2]}', file_content) #Modify the Value
+        command = ['sudo', '-S', 'tee', self.data[0]]
+        process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True) #Rewrite the file content with the modified value
+        process.communicate(input=modified_content)
+        process.wait()
+        os.system(self.data[3]) #Execute the command
+        time.sleep(self.data[4]) #Wait for the changes 
